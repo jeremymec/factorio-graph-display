95a7fc8c5d579c1b29c4209521989a3c10c1810d
Jeremy Mechen <jeremymechen1997@gmail.com>
2021-08-10 01:32:57 +1200

    Remove isLoading from props

--- renderedGraph.tsx
+++ renderedGraph.tsx
@@ -2,50 +2,45 @@ import React from 'react';
 import { processGraph, highlightLine, removeHighlightLine, FactorioNode, Line } from './graphDisplay'
 
 interface RenderedGraphProps {
-    nodes: FactorioNode[],
-    isLoading: boolean
+    nodes: FactorioNode[]
 }
    
-  const RenderedGraph = ({ nodes, isLoading }: RenderedGraphProps) => {
+  const RenderedGraph = ({ nodes }: RenderedGraphProps) => {
 
     const canvas = React.useRef(null)
     const [lines, setLines] = React.useState<Line[]>(null)
 
     React.useEffect(() => {
-
-        if (!isLoading) {
-
-          const mouseoverLineCallback = (event: MouseEvent) => {
-            
-            for (let line of lines) {
-              let currentlyHighlited = false
-              for (let path of line.paths) {
-                if (canvas.current.getContext('2d').isPointInStroke(path, event.offsetX, event.offsetY)) {
-                  highlightLine(canvas.current.getContext('2d'), line)
-                  currentlyHighlited = true
-                }
+        const mouseoverLineCallback = (event: MouseEvent) => {
+          
+          for (let line of lines) {
+            let currentlyHighlited = false
+            for (let path of line.paths) {
+              if (canvas.current.getContext('2d').isPointInStroke(path, event.offsetX, event.offsetY)) {
+                highlightLine(canvas.current.getContext('2d'), line)
+                currentlyHighlited = true
               }
-              if (!currentlyHighlited) {
-                removeHighlightLine(canvas.current.getContext('2d'), line)
-              }
-              currentlyHighlited = false
             }
+            if (!currentlyHighlited) {
+              removeHighlightLine(canvas.current.getContext('2d'), line)
+            }
+            currentlyHighlited = false
           }
-
-          const ctx = canvas.current.getContext('2d')
-          canvas.current.addEventListener("mousemove", mouseoverLineCallback, false)
-
-          const lines = processGraph(ctx, nodes)
-          console.log("Lines", lines)
-          setLines(lines)
         }
 
-    }, [nodes, isLoading])
+        const ctx = canvas.current.getContext('2d')
+        canvas.current.addEventListener("mousemove", mouseoverLineCallback, false)
+
+        const lines = processGraph(ctx, nodes)
+        console.log("Lines", lines)
+        setLines(lines)
+
+    }, [nodes])
 
 
 
 
-    return <canvas ref={canvas} width={1000} height={1000} />
+    return <canvas ref={canvas} width={2000} height={1000} />
   }
     
 export default RenderedGraph;
